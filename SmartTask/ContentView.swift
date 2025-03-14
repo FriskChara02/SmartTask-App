05f4ec1cb187de6f976129c2afb786b3cf288621
FriskChara02 <loi.nguyenbao02@gmail.com>
2025-03-14 13:12:53 +0700

    Initial Commit

---
+++ SmartTask/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SmartTask
+//
+//  Created by Loi Nguyen on 14/3/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SmartTask/SmartTaskApp.swift
@@ -0,0 +1,17 @@
+//
+//  SmartTaskApp.swift
+//  SmartTask
+//
+//  Created by Loi Nguyen on 14/3/25.
+//
+
+import SwiftUI
+
+@main
+struct SmartTaskApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
